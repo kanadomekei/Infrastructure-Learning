71713896c5d1b079e3f72f7a1d4ccaae8eac0d48
kanadome <tsuchiyakeita2025@gmail.com>
2024-06-03 18:02:07 +0900

    first commit

---
+++ さわって学ぶクラウドインフラ/infra.tf
@@ -0,0 +1,203 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "public_instance" {
+  ami                         = "ami-02a405b3302affc24" # Amazon Linux 2023 AMI ID
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.my_key.key_name
+  vpc_security_group_ids      = [aws_security_group.ssh_sg.id]
+  availability_zone           = "ap-northeast-1a"
+  private_ip                  = "10.0.1.10"
+
+  tags = {
+    Name = "public-instance"
+  }
+}
+
+resource "aws_security_group" "ssh_sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh-sg"
+  }
+}
+
+resource "aws_ebs_volume" "my_volume" {
+  availability_zone = "ap-northeast-1a"
+  size              = 10
+
+  tags = {
+    Name = "my-volume"
+  }
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.my_volume.id
+  instance_id = aws_instance.public_instance.id
+}
+
+resource "aws_security_group" "db_sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] # VPC内からのアクセスを許可
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["10.0.0.0/16"] # VPC内からのICMPを許可
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22    
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24"] # パブリックサブネットからのSSHアクセスを許可
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "db-sg"
+  }
+}
+
+resource "aws_instance" "private_instance" {
+  ami                         = "ami-02a405b3302affc24" 
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.private_subnet.id
+  associate_public_ip_address = false
+  key_name                    = aws_key_pair.my_key.key_name
+  vpc_security_group_ids      = [aws_security_group.db_sg.id]
+  availability_zone           = "ap-northeast-1a"
+  private_ip                  = "10.0.2.10"
+
+  tags = {
+    Name = "DB-server"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
